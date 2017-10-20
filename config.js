ece2f64f33f8424082a75853cb513bdddf84290f
Hafiz Suara <hafizsuara@Hafizs-MBP.hitronhub.home>
2017-10-20 16:54:43 -0600

    added db production config

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
     'tokenSecret': 'ilovemesecreet',
     'db': {
         'development': 'mongodb://localhost:27017/authServer',
+        'production': 'mongodb://0.0.0.0:27017/authServer',
         'test': 'mongodb://localhost:27017/authServerTest'
     }[process.env.NODE_ENV || 'development']
 
