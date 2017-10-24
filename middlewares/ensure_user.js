c1c8e691e5e8713d094cf02b4ccdc4f6d6bdabf0
Hafiz Suara <hafizsuara@Hafizs-MacBook-Pro.local>
2017-10-23 20:12:57 -0600

    debuggin ensureUser

--- middlewares/ensure_user.js
+++ middlewares/ensure_user.js
@@ -4,11 +4,12 @@ const tokenSecret = require('../config').tokenSecret;
 module.exports = ({ User }) => async (ctx, next) => {
   ctx.body = ctx.body || {}
   const token = ctx.body.token || ctx.query.token || ctx.headers['x-access-token'];
-
+  console.log('token in middleware', token)
   try {
-   // jsonwebtoken should throw if can't verify https://github.com/auth0/node-jsonwebtoken
-   const decoded = jwt.verify(token, tokenSecret);
-   const user = await User.findOne({ '_id': decoded.userId });
+    // jsonwebtoken should throw if can't verify https://github.com/auth0/node-jsonwebtoken
+    const decoded = jwt.verify(token, tokenSecret);
+    const user = await User.findOne({ '_id': decoded.userId });
+    console.log('token in middleware', user, 'lastActive', user.lastActive)
    if (!user) throw new Error('user not found');
    if (user.lastActive != decoded.lastActive) throw new Error('stale user');
     ctx.user = user;
